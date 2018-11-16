c86c1de09eb662aba50e9a7bf9fd488c61559485
firestanf <goolman1231@gmail.com>
2018-11-16 16:48:54 +0700

    Login_register_basic

---
+++ client/src/views/Dashboard_Dokter.vue
@@ -0,0 +1,14 @@
+<template>
+<h2>DashBoard Dokter</h2>
+</template>
+
+
+<script>
+export default {
+      name:'dashboard_dokter',
+      data(){
+            return{
+                  }
+      }
+}
+</script>
