fea5091f725e4a5a07b75099d5d969733f0504b0
cantdocpp <cihuyman95@gmail.com>
2018-11-17 02:23:00 +0700

    fixing user service

--- client/src/libraries/service/user.service.js
+++ client/src/libraries/service/user.service.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-export default function(Vue) {
-  Vue.request = {
-    testConnect() {
-      console.log("From Auth");
-    },
-    Reg_user() {
-      return axios.get("/");
-    },
-    Login_user(email, password) {
-      return axios.post("/patients/login", {
-        email: email,
-        password: password
-      });
-=======
 export default function (Vue){
     Vue.request={
         testConnect(){
@@ -27,7 +12,6 @@ export default function (Vue){
         Daftar_jadwal(infodokter,user_info,order_info){
             return axios.post('/',{info_dokter:infodokter,info_user:user_info,info_order:order_info});
         }
->>>>>>> d2a2674a615c7127be66e6e0ecf07d1b52098ddc
     }
   };
   Object.defineProperties(Vue.prototype, {
